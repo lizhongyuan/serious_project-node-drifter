62976edf0c3b4c7edbc6454e688e997497947cc5
lizhongyuan <hybridblader@163.com>
2016-03-10 17:45:33 +0800

    delete the node_modeules in reposity, change app.js and router/index.js

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/users', users);
+
+routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
